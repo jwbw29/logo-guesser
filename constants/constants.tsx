084ae337d30721e3e40041aaebff762d4fac1122
Justin Byrd <justinbyrd7@gmail.com>
2024-02-24 21:25:00 -0600

    Refactor code for improved performance and readability

---
+++ constants/constants.tsx
@@ -0,0 +1,95 @@
+//Leagues
+export const NBA = "NBA";
+export const NFL = "NFL";
+export const MLB = "MLB";
+export const MLS = "MLS";
+export const NHL = "NHL";
+export const WNBA = "WNBA";
+export const NCAA = "NCAA";
+
+//Conferences
+//NBA/NHL/WNBA/MLS
+export const WESTERN_CONFERENCE = "Western Conference";
+export const EASTERN_CONFERENCE = "";
+
+//MLB
+export const NATIONAL_LEAGUE = "National League";
+export const AMERICAN_LEAGUE = "American League";
+
+//NFL
+export const NFC = "NFC";
+export const AFC = "AFC";
+
+//NCAA
+export const ACC = "ACC";
+export const BIG_10 = "Big 10";
+export const BIG_12 = "Big 12";
+export const PAC_12 = "Pac 12";
+export const SEC = "SEC";
+
+//Divisions
+export const ATLANTIC = "Atlantic";
+export const CENTRAL = "Central";
+export const SOUTHEAST = "Southeast";
+export const NORTHWEST = "Northwest";
+export const PACIFIC = "Pacific";
+export const SOUTHWEST = "Southwest";
+export const EAST = "East";
+export const WEST = "West";
+export const NORTH = "North";
+export const SOUTH = "South";
+export const METROPOLITAN = "Metropolitan";
+
+//States
+export const AL = "Alabama";
+export const AK = "Alaska";
+export const AZ = "Arizona";
+export const AR = "Arkansas";
+export const CA = "California";
+export const CO = "Colorado";
+export const CT = "Connecticut";
+export const DC = "Washington D.C.";
+export const DE = "Delaware";
+export const FL = "Florida";
+export const GA = "Georgia";
+export const HI = "Hawaii";
+export const ID = "Idaho";
+export const IL = "Illinois";
+export const IN = "Indiana";
+export const IA = "Iowa";
+export const KS = "Kansas";
+export const KY = "Kentucky";
+export const LA = "Louisiana";
+export const ME = "Maine";
+export const MD = "Maryland";
+export const MA = "Massachusetts";
+export const MI = "Michigan";
+export const MN = "Minnesota";
+export const MS = "Mississippi";
+export const MO = "Missouri";
+export const MT = "Montana";
+export const NE = "Nebraska";
+export const NV = "Nevada";
+export const NH = "New Hampshire";
+export const NJ = "New Jersey";
+export const NM = "New Mexico";
+export const NY = "New York";
+export const NC = "North Carolina";
+export const ND = "North Dakota";
+export const OH = "Ohio";
+export const OK = "Oklahoma";
+export const OR = "Oregon";
+export const PA = "Pennsylvania";
+export const RI = "Rhode Island";
+export const SC = "South Carolina";
+export const SD = "South Dakota";
+export const TN = "Tennessee";
+export const TX = "Texas";
+export const UT = "Utah";
+export const VT = "Vermont";
+export const VA = "Virginia";
+export const WA = "Washington";
+export const WV = "West Virginia";
+export const WI = "Wisconsin";
+export const WY = "Wyoming";
+export const CAN = "Canada";
